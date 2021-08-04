a97f7c2685b0b62a3985e1f9e2a7b5defddb66cb
thaoktk <tranthuthao.bhn.96@gmail.com>
2021-08-04 09:56:17 +0700

    update-2

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+setTimeout(function () {
+    alert('Make sure that you\'ve turned off the UNIKEY to use the keyboard')
+}, 1000)
 
 var numberOfDrumButtons = document.querySelectorAll('.drum').length
 
@@ -17,45 +19,54 @@ document.addEventListener('keypress', function(e) { // cả cái document gọi 
 
     makeSound(e.key)
     buttonAnimation(e.key)
-});
 
+    // khi bấm phím nhớ tắt unikey, ví dụ chữ w mà để unikey sẽ thành ư => báo lỗi
+});
 
 function makeSound(key) {
     switch (key) {
         case 'w': 
-            var tom1 = new Audio("./assets/sounds/tom-1.mp3")
-            tom1.play();
+            playSound("./assets/sounds/tom-1.mp3");
             break;
         case 'a': 
-            var tom2 = new Audio("./assets/sounds/tom-2.mp3")
-            tom2.play();
+            playSound("./assets/sounds/tom-2.mp3")
             break;
         case 's': 
-            var tom3 = new Audio("./assets/sounds/tom-3.mp3")
-            tom3.play();
+            playSound("./assets/sounds/tom-3.mp3")
             break;
         case 'd': 
-            var tom4 = new Audio("./assets/sounds/tom-4.mp3")
-            tom4.play();
+            playSound("./assets/sounds/tom-4.mp3")
             break;
         case 'j': 
-            var snare = new Audio("./assets/sounds/snare.mp3")
-            snare.play();
+            playSound("./assets/sounds/snare.mp3")
             break;
         case 'k': 
-            var crash = new Audio("./assets/sounds/crash.mp3")
-            crash.play();
+            playSound("./assets/sounds/crash.mp3")
             break;
         case 'l': 
-            var kick = new Audio("./assets/sounds/kick-bass.mp3")
-            kick.play();
+            playSound("./assets/sounds/kick-bass.mp3")
             break;
     }
+}
+
+function playSound(link) {
+    var sound = new Audio(link);
+    sound.play();
+
+    sound.onended = function(){
+        this.currentSrc = null;
+        this.src = "";
+        this.srcObject = null;
+        this.remove();
+    };
+
+    // fix lỗi Blocked attempt to create a WebMediaPlayer as there are too many WebMediaPlayers already in existence
 }
 
 function buttonAnimation(currentKey) {
     var activeButton = document.querySelector('.' + currentKey)
     activeButton.classList.add('pressed')
+
     setTimeout(() => {
         activeButton.classList.remove('pressed')
     }, 100)
